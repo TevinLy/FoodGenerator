84501f34c2d5774a2ea93cde55cc9c98305f0ce0
Tevin Ly <tevinly97@gmail.com>
2020-06-30 22:58:07 -0700

    Initial setup
    Initial app setup with NPM

---
+++ app.js
@@ -0,0 +1,12 @@
+// FoodGenerator 
+
+var express = require ("express");
+var app = express () ; 
+var request = require ( "request");
+
+
+
+app.listen (3000 , function () {
+    console.log ( "FoodGenerator started");
+});
+
