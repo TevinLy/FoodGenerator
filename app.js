71d1a5f442d054c399a25611bb977e8641cbba67
Tevin Ly <tevinly97@gmail.com>
2020-07-01 14:58:44 -0700

    Initial landing page
    Added EJS and initial landing page

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var express = require ("express");
 var app = express () ; 
 var request = require ( "request");
 
+app.set ( "view engine" , "ejs");
+
+
+//landing route
+app.get("/", function (req,res){
+    res.render ( "landing");
+});
 
 
 app.listen (3000 , function () {
