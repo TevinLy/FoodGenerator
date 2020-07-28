596d31fda809ad1b203d1d28bfc792086c7ca493
Tevin Ly <tevinly97@gmail.com>
2020-07-28 14:30:37 -0700

    clean-up apiCall(), etc
    Turned apiCall into its own function. Created displayData function. Added comments.

--- apiCall.js
+++ apiCall.js
@@ -1,23 +1,61 @@
+//Using axios to make GET request to Yelp API
+
 const axios = require('axios')
 
+/*
+** Name : apiCall
+** Parameters: terms (type = dict) - search terms/parameters used for our search. see below for format
+** Returns: 0 if error, 1 if everything is good and we moved to displayData()
+** Description: Given search parameters, we call the YelpFusion API. Data is passed to 
+** displayData() function ins displayData.js. NO ERROR HANDLING. MUST HANDLE IN CALLER FNC
+*/
 
+/*
+** Dictionary used to organize parameters. 
+** search terms example. 
+**  terms = {
+        term:'Tacos',
+        location: 'castro valley, ca',
+        limit : '3',
+        open_now: 'true'
+**  };
+** Please take a look at *insertParamsTextFile.txt* for detailed info
+*/
 
-url = 'https://api.yelp.com/v3/businesses/search'
-clientID='Uc7Xq3H9mbGVve2Zj7a9RQ'
+function apiCall ( terms )
+{
+    const url = 'https://api.yelp.com/v3/businesses/search'
 
-header ={'Authorization': 'Bearer '}
+    // api key in format 'Bearer <API_KEY>' //please keep dev api off git
+    header ={'Authorization': 'Bearer '}
 
-
-// search terms
-const terms = {
-    term:'Tacos',
-    location: 'castro valley, ca'
-  };
+    // search terms example. 
+    terms = {
+        term:'Tacos',
+        location: 'castro valley, ca',
+        limit : '3',
+        open_now: 'true'
+    };
 
   axios.get(url, {headers:header, params:terms})
   .then(response => {
-    console.log(response)
+        //console.log(response.data)
+        let results = [];
+        for ( let business of response.data.businesses)
+        {
+            // console.log(business)
+            results.push(business)
+        }
+        console.log(results)
+        //Call display results if we dont get error
+
+        return 1;
   })
   .catch(error => {
     console.log(error)
   })
+  return 0;
+}
+
+
+
---
+++ displayData.js
@@ -0,0 +1,16 @@
+
+
+/*
+** Name : displayData
+** Parameters: results (type = array) - results from apiCall()
+** Returns: None
+** Description: Given data, we display it by rendering an new page for our user
+** Error handling will be done here. Errors are estimated to only come from empty results data
+** or Error displaying webpage
+*/
+
+function displayData( results )
+{
+
+
+}
