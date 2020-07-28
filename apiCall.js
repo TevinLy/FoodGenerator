373f28c9a89bc5667f0982aa7cb0fee9cfb0ed34
Tevin Ly <tevinly97@gmail.com>
2020-07-28 13:03:33 -0700

    removed sensitive info

--- apiCall.js
+++ apiCall.js
@@ -4,8 +4,8 @@ const axios = require('axios')
 
 url = 'https://api.yelp.com/v3/businesses/search'
 clientID='Uc7Xq3H9mbGVve2Zj7a9RQ'
-apiKey = '8f-9kzIemVxck4ONpD8gVflODiELG0kqJCwhqgw7f9cTgqza1wLfkvTBmwqhyzfRJLL9K7-cKdoz0JT3le2VR1S40z51tw4FsLN88RSILB_zEIJ2p_rn4L_tIHYgX3Yx'
-header ={'Authorization': 'Bearer 8f-9kzIemVxck4ONpD8gVflODiELG0kqJCwhqgw7f9cTgqza1wLfkvTBmwqhyzfRJLL9K7-cKdoz0JT3le2VR1S40z51tw4FsLN88RSILB_zEIJ2p_rn4L_tIHYgX3Yx'}
+
+header ={'Authorization': 'Bearer '}
 
 
 // search terms
